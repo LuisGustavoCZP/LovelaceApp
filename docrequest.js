966a67b84a0bcba299583c5424694993bc5dcf23
LuisGustavoCZP <luisgustavoczp@gmail.com>
2021-10-09 17:12:58 -0300

    separando codigo de html

---
+++ docrequest.js
@@ -0,0 +1,45 @@
+function processElement (element, parent)
+{
+    var newTag = document.createElement(element.tagName);
+    newTag.innerText = element.innerText;
+    parent.appendChild(newTag);
+
+    for(j = 0; j < element.childElementCount; j++)
+    {
+        processElement (element.children[j], newTag);
+    }
+}
+
+function processDocData(doc) 
+{
+    var elements = doc.body;
+    console.log(elements.childElementCount.toString());
+
+    for(i = 0; i < elements.childElementCount; i++)
+    {
+        parent.appendChild(elements.children[i]);
+        //processElement (elements[i], informacoes);
+    }
+}
+
+function readHandler(evt) 
+{
+    if(this.status == 200 && this.response != null) 
+    {
+        // success!
+        processDocData(evt.response);
+        
+    } 
+    else
+    {
+        
+    }
+}
+
+function readDoc (filePath)
+{
+    var client = new XMLHttpRequest();
+    client.onload = handler;
+    client.open("GET", filePath);
+    client.send();
+}
